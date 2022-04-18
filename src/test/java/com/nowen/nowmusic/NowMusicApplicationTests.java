0a08757fd1094667cd6d2fc68f5d14e03e661694
zhaozhiwen <1497074172@qq.com>
2022-04-18 17:27:41 +0800

    frist commit

---
+++ src/test/java/com/nowen/nowmusic/NowMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowen.nowmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
