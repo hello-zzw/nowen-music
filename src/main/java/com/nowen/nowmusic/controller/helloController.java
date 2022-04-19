8b6eb1db6ce64a96e16f5b69f61649aad1607bfc
zhaozhiwen <1497074172@qq.com>
2022-04-19 15:59:38 +0800

    跨域问题

--- src/main/java/com/nowen/nowmusic/controller/helloController.java
+++ src/main/java/com/nowen/nowmusic/controller/helloController.java
@@ -1,5 +1,6 @@
 package com.nowen.nowmusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/4/18
  */
 @RestController
+@CrossOrigin
 public class helloController {
 
     @GetMapping("/hello")
